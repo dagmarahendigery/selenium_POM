3a933669e1e4ae5ee596b1c2d83941c30ccbb2e4
dagmarahendigery <dagmara.hendigery@gmail.com>
2026-03-15 09:47:40 +0000

    add

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,8 +3,9 @@ class BasePage:
     Base Page Object for each page
     """
     def __init__(self, driver):
-        self.driver = driver #self imie =imie ; konstruktor
+        self.driver = driver
         self._verify_page()
 
-    def _verify_page(self): #site autotest
+    def _verify_page(self):
+        # site autotest
         return
--- pages/home_page.py
+++ pages/home_page.py
@@ -1,6 +1,6 @@
 from pages.base_page import BasePage
+from pages.authentication_page import AuthenticationPage
 from selenium.webdriver.common.by import By
-
 
 class Locators:
     """
@@ -13,5 +13,10 @@ class HomePage(BasePage):
     Home Page Object
     """
     def click_sign_in(self):
+        """
+        Clicks Sign In and goes to Authentication Page
+        :return: AuthenticationPage Object
+        """
         self.driver.find_element(*Locators.SIGN_IN_LINK).click()
+        return AuthenticationPage(self.driver)
 
